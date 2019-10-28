ff3917ef91e8a82e8655dd6fad35d0c6163f36bf
mathugo <hugo.mathh@gmail.com>
2019-10-28 15:18:26 +0000

    build.sh updated

--- build.sh
+++ build.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
-javac server_class/*.java
-javac main.java
-echo "[*] Done building"
-chmod +x main.class
-#java main
+jarName="MauveS.jar"
+cd src/
+
+javac -d ../bin/ serverPackage/*.java
+
+if [ $? -eq 0 ]; then
+    echo "[*] Done building package"
+    javac -d ../bin/ main.java
+    if [ $? -eq 0 ]; then
+      echo "[*] Done building"
+      chmod +x ../bin/main.class
+      echo "[!] Creating jar file .."
+      jar cvf ../$jarName ../bin/*.class ../bin/*/*.class
+        if [ $? -eq 0 ]; then
+          echo "[*] Done"
+        else
+          echo "[!] Error creating jar file"
+        fi
+    else
+      echo "[!] Failed building main"
+    fi
+else
+  echo "[!] Failed building packages"
+fi

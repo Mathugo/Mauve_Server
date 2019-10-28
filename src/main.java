8a6b2160ef435c7efb78d88de96e1e4e53d2ae22
mathugo <hugo.mathh@gmail.com>
2019-10-28 07:31:06 +0000

    build.sh

---
+++ src/main.java
@@ -0,0 +1,27 @@
+import serverPackage.*;
+
+import java.util.*;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+
+public class main
+{
+  public static void main(String[] args)
+  {
+
+    int port=32889;
+    Server ser = new Server(port);
+    ser.start();
+
+  //  ser.recv(ser.getClients().get(0));
+  //  ser.printBuffer();
+
+
+  }
+
+}
